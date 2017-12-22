0d2271ccb4f35f56300dd50522d54bd4f11aa3ae
Gianfalzo91 <34582544+Gianfalzo91@users.noreply.github.com>
2017-12-22 12:23:58 +0100

    Add files via upload

--- CNMining.java
+++ CNMining.java
@@ -699,20 +699,21 @@ public class CNMining {
         CLIContext context = new CLIContext();
         CLIPluginContext pluginContext = new CLIPluginContext(context, "test");
         ProMJGraphPanel mainPanel = ProMJGraphVisualizer.instance().visualizeGraph(pluginContext, flex);
-
         mainPanel.setSize(new Dimension(500, 500));
-
         ProMJGraph graph = (ProMJGraph) mainPanel.getComponent();
         graph.setSize(new Dimension(500, 500));
     }
 
     private void removeStrangeDependencies(Graph g, ObjectIntOpenHashMap<String> map, ObjectArrayList<Constraint> vincoli_positivi) {
-        for (int ii = 0; ii < g.listaNodi().size(); ii++) {
+        int gListaNodiSize = g.listaNodi().size();
+        int gAdjacentNodes;
+        for (int ii = 0; ii < gListaNodiSize; ii++) {
             Node n = (Node) g.listaNodi().get(ii);
             g.removeEdge(n, n);
             n.decr_Outer_degree();
             n.decr_Inner_degree();
-            for (int jj = 0; jj < g.adjacentNodes(n).size(); jj++) {
+            gAdjacentNodes = g.adjacentNodes(n).size();
+            for (int jj = 0; jj < gAdjacentNodes; jj++) {
                 Node adjNode = (Node) g.listaNodi().get(jj);
 
                 if (n.getNomeAttivita().split("_")[1].split("\\+")[0].equals(adjNode.getNomeAttivita().split("_")[0])) {
@@ -722,7 +723,9 @@ public class CNMining {
                     n.decr_Outer_degree();
                     adjNode.decr_Inner_degree();
                 }
+                gAdjacentNodes = g.adjacentNodes(n).size();
             }
+            gListaNodiSize = g.listaNodi().size();
         }
 
         Node pb = new Node("via panebianco_via busento (rende 1o fermata)+complete",
@@ -734,10 +737,12 @@ public class CNMining {
 
     public boolean[][] generaAdjacentsMatrix(Graph folded_g) {
         boolean[][] adjacentsMatrix = new boolean[folded_g.listaNodi().size()][folded_g.listaNodi().size()];
-
-        for (int i = 0; i < folded_g.listaNodi().size(); i++) {
+        int foldedGListaNodiSize = folded_g.listaNodi().size();
+        int foldedGAdjacentNodesSize;
+        for (int i = 0; i < foldedGListaNodiSize; i++) {
             Node n = (Node) folded_g.listaNodi().get(i);
-            for (int j = 0; j < folded_g.adjacentNodes(n).size(); j++) {
+            foldedGAdjacentNodesSize = folded_g.adjacentNodes(n).size();
+            for (int j = 0; j < foldedGAdjacentNodesSize); j++) {
                 Node adjacent = (Node) folded_g.adjacentNodes(n).get(j);
                 adjacentsMatrix[n.getID_attivita()][adjacent.getID_attivita()] = true;
             }
@@ -746,9 +751,12 @@ public class CNMining {
     }
 
     public boolean verifica_consistenza_vincoli(ObjectArrayList<Constraint> vincoli_positivi, ObjectArrayList<Constraint> vincoli_negati) {
-        for (int i = 0; i < vincoli_positivi.size(); i++) {
+        int vincoliPositiviSize = vincoli_positivi.size();
+        int vincoliNegatiSize;
+        for (int i = 0; i < vincoliPositiviSize; i++) {
             Constraint c = (Constraint) vincoli_positivi.get(i);
-            for (int j = 0; j < vincoli_negati.size(); j++) {
+            vincoliNegatiSize = vincoli_negati.size();
+            for (int j = 0; j < vincoliNegatiSize; j++) {
                 Constraint f = (Constraint) vincoli_negati.get(j);
                 if ((c.equals(f)) && (((c.isPathConstraint()) && (f.isPathConstraint())) || ((!c.isPathConstraint()) && (!f.isPathConstraint()))))
                     return false;
